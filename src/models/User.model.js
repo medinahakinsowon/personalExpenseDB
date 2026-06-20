ba73376865ff728653b82dedee4cf8d53000a583
medinah <ethusiast.medinah@cloudethusiat.com>
2026-06-20 10:09:01 +0100

    modified the user schema

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -34,11 +34,10 @@ const userSchema = new mongoose.Schema(
 );
 
 // Hash the password before saving, only if it was changed
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
   const salt = await bcrypt.genSalt(12);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
  
 // Instance method to compare a plaintext password against the stored hash
